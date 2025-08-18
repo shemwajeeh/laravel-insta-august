a5a1353bcc9dc8c37643ba32545b4bff36ab04fd
chimchim <kredoteacher.oswdelosreyes@gmail.com>
2025-08-18 16:26:57 +0800

    changing design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,21 +12,27 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     {{-- <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet"> --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <h1 class="h5 mb-0">{{ config('app.name') }}</h1>
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <span class="brand-text">{{ config('app.name') }}</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -35,12 +42,12 @@
                     @auth
                         {{-- This will not show up in the admin side --}}
                         @if (!request()->is('admin/*'))
-                        <ul class="navbar-nav ms-auto">
-                            <form action="{{ route('search') }}" style="width:300px">
-                                <input type="search" name="search" class="form-control form-control-sm" placeholder="Search...">
-                            </form>
-                        </ul>
-                            
+                            <ul class="navbar-nav ms-auto">
+                                <form action="{{ route('search') }}" style="width:300px">
+                                    <input type="search" name="search" class="form-control form-control-sm"
+                                        placeholder="Search...">
+                                </form>
+                            </ul>
                         @endif
                     @endauth
 
@@ -48,7 +55,7 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
+                            {{-- @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
@@ -58,23 +65,26 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             {{-- Home --}}
                             <li class="nav-item" title="Home">
-                                <a href="{{ route('index') }}" class="nav-link"><i class="fa-solid fa-house text-dark icon-sm"></i></a>
+                                <a href="{{ route('index') }}" class="nav-link"><i
+                                        class="fa-solid fa-house text-dark icon-sm"></i></a>
                             </li>
 
                             {{-- Create Post --}}
                             <li class="nav-item" title="Create Post">
-                                <a href="{{ route('post.create') }}" class="nav-link"><i class="fa-solid fa-circle-plus text-dark icon-sm"></i></a>
+                                <a href="{{ route('post.create') }}" class="nav-link"><i
+                                        class="fa-solid fa-circle-plus text-dark icon-sm"></i></a>
                             </li>
 
                             {{-- Account --}}
                             <li class="nav-item dropdown">
                                 <button id="account-dropdown" class="btn shadow-none nav-link" data-bs-toggle="dropdown">
                                     @if (Auth::user()->avatar)
-                                        <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" class="rounded-circle avatar-sm">
+                                        <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}"
+                                            class="rounded-circle avatar-sm">
                                     @else
                                         <i class="fa-solid fa-circle-user text-dark icon-sm"></i>
                                     @endif
@@ -84,14 +94,12 @@
                                     {{-- [SOON] Admin Controls --}}
                                     {{-- @can('admin') --}}
                                     @if (Gate::allows('admin'))
-                                    
-                                    
-                                    <a href="{{ route('admin.users') }}" class="dropdown-item">
-                                        <i class="fa-solid fa-user-gear"></i> Admin
-                                    </a>
+                                        <a href="{{ route('admin.users') }}" class="dropdown-item">
+                                            <i class="fa-solid fa-user-gear"></i> Admin
+                                        </a>
 
-                                    <hr class="dropdown-divider">
-                                    {{-- @endcan --}}
+                                        <hr class="dropdown-divider">
+                                        {{-- @endcan --}}
                                     @endif
 
                                     {{-- Profile --}}
@@ -101,7 +109,7 @@
 
                                     {{-- Logout --}}
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         <i class="fa-solid fa-right-from-bracket"></i> {{ __('Logout') }}
                                     </a>
@@ -124,10 +132,12 @@
                     @if (request()->is('admin/*'))
                         <div class="col-3">
                             <div class="list-group">
-                                <a href="{{ route('admin.users') }}" class="list-group-item {{ request()->is('admin/users') ? 'active' : '' }}">
+                                <a href="{{ route('admin.users') }}"
+                                    class="list-group-item {{ request()->is('admin/users') ? 'active' : '' }}">
                                     <i class="fa-solid fa-users"></i> Users
                                 </a>
-                                <a href="{{ route('admin.posts') }}" class="list-group-item {{ request()->is('admin/posts') ? 'active' : '' }}">
+                                <a href="{{ route('admin.posts') }}"
+                                    class="list-group-item {{ request()->is('admin/posts') ? 'active' : '' }}">
                                     <i class="fa-solid fa-newspaper"></i> Posts
                                 </a>
                                 <a href="{{ route('admin.categories') }}"
@@ -146,4 +156,5 @@
         </main>
     </div>
 </body>
+
 </html>
