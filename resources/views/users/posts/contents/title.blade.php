f4e405043a6dd1c6dcdc6f9abcdc0abbe869c971
chimchim <kredoteacher.oswdelosreyes@gmail.com>
2025-08-18 17:03:35 +0800

    changing home design

--- resources/views/users/posts/contents/title.blade.php
+++ resources/views/users/posts/contents/title.blade.php
@@ -1,46 +1,56 @@
-<div class="card-header bg-white py-3">
-    <div class="row align-items-center">
-        <div class="col-auto">
+<div class="card-header border-0 py-3" style="background-color:#FAF9EE; border-radius: 15px;">
+    <div class="d-flex justify-content-between align-items-center">
+        
+        {{-- Left side: avatar + name --}}
+        <div class="d-flex align-items-center">
             <a href="{{ route('profile.show', $post->user->id) }}">
                 @if ($post->user->avatar)
-                    <img src="{{ $post->user->avatar }}" alt="{{ $post->user->name }}" class="rounded-circle avatar-sm">
+                    <img src="{{ $post->user->avatar }}" 
+                         alt="{{ $post->user->name }}" 
+                         class="rounded-circle avatar-sm me-2 border" 
+                         style="border:2px solid #A2AF9B;">
                 @else
-                    <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
+                    <i class="fa-solid fa-circle-user text-secondary icon-sm me-2"></i>
                 @endif
             </a>
-        </div>
-        <div class="col ps-0">
-            <a href="{{ route('profile.show', $post->user->id) }}" class="text-decoration-none text-dark">{{ $post->user->name }}</a>
-        </div>
-        <div class="col-auto">
-            <div class="dropdown">
-                <button class="btn btn-sm shadow-none" data-bs-toggle="dropdown">
-                    <i class="fa-solid fa-ellipsis"></i>
-                </button>
-                {{-- if you are the owner, you can edit and delete --}}
-                @if (Auth::user()->id === $post->user->id)
-                    <div class="dropdown-menu">
-                        <a href="{{ route('post.edit', $post->id) }}" class="dropdown-item">
-                            <i class="fa-regular fa-pen-to-square"></i> Edit
-                        </a>
-                        <button class="dropdown-item text-danger" data-bs-toggle="modal" data-bs-target="#delete-post-{{ $post->id }}">
-                            <i class="fa-regular fa-trash-can"></i> Delete
-                        </button>
-                    </div>
-                    {{-- Include modal here --}}
-                    @include('users.posts.contents.modals.delete')
-                @else
-                {{-- if you are not the owner, show unfollow button --}}
-                    <div class="dropdown-menu">
-                        <form action="{{ route('follow.destroy', $post->user->id) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-
-                            <button type="submit" class="dropdown-item text-danger">Unfollow</button>
-                        </form>
-                    </div>
-                @endif
+            <div>
+                <a href="{{ route('profile.show', $post->user->id) }}" 
+                   class="fw-bold text-decoration-none" 
+                   style="color:#4A4A4A;">{{ $post->user->name }}</a>
+                <p class="mb-0 small text-muted">Posted {{ $post->created_at->diffForHumans() }}</p>
             </div>
+        </div>
+
+        {{-- Right side: dropdown menu --}}
+        <div class="dropdown">
+            <button class="btn btn-sm shadow-none text-muted" data-bs-toggle="dropdown">
+                <i class="fa-solid fa-ellipsis"></i>
+            </button>
+
+            @if (Auth::user()->id === $post->user->id)
+                <div class="dropdown-menu dropdown-menu-end shadow-sm rounded-3">
+                    <a href="{{ route('post.edit', $post->id) }}" class="dropdown-item">
+                        <i class="fa-regular fa-pen-to-square me-1"></i> Edit
+                    </a>
+                    <button class="dropdown-item text-danger" 
+                            data-bs-toggle="modal" 
+                            data-bs-target="#delete-post-{{ $post->id }}">
+                        <i class="fa-regular fa-trash-can me-1"></i> Delete
+                    </button>
+                </div>
+                {{-- Include delete modal --}}
+                @include('users.posts.contents.modals.delete')
+            @else
+                <div class="dropdown-menu dropdown-menu-end shadow-sm rounded-3">
+                    <form action="{{ route('follow.destroy', $post->user->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="dropdown-item text-danger">
+                            <i class="fa-solid fa-user-xmark me-1"></i> Unfollow
+                        </button>
+                    </form>
+                </div>
+            @endif
         </div>
     </div>
 </div>
